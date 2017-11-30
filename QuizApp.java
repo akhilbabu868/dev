9f941b0885377459869fd86045791830cdbedd6f
Akhil Babu <akhilbabu868@gmail.com>
2017-11-30 14:38:20 +0530

    Simple Quiz app
    It  is a  simple swing Desktop applictaion

---
+++ QuizApp.java
@@ -0,0 +1,17 @@
+
+package quiz.app;
+
+/**
+ *
+ * @author AB-2077
+ */
+public class QuizApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
